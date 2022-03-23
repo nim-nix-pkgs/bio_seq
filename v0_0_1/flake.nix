59ae691955230901c8de3dbc0591f7cd36b28719
hugosenari <hugosenari@gmail.com>
2022-03-23 19:07:34 -0300

    feat(nimpkgs): remove lock

--- v0_0_1/flake.nix
+++ v0_0_1/flake.nix
@@ -7,19 +7,19 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-bio_seq-v0_0_1.flake = false;
-  inputs.src-bio_seq-v0_0_1.ref   = "refs/tags/v0.0.1";
-  inputs.src-bio_seq-v0_0_1.owner = "kerrycobb";
-  inputs.src-bio_seq-v0_0_1.repo  = "BioSeq";
-  inputs.src-bio_seq-v0_0_1.type  = "github";
+  inputs.src-nim_seq-v0_0_1.flake = false;
+  inputs.src-nim_seq-v0_0_1.ref   = "refs/tags/v0.0.1";
+  inputs.src-nim_seq-v0_0_1.owner = "kerrycobb";
+  inputs.src-nim_seq-v0_0_1.repo  = "BioSeq";
+  inputs.src-nim_seq-v0_0_1.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-bio_seq-v0_0_1"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nim_seq-v0_0_1"];
   in lib.mkRefOutput {
     inherit self nixpkgs ;
-    src  = deps."src-bio_seq-v0_0_1";
+    src  = deps."src-nim_seq-v0_0_1";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   };
